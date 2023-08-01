c0492210ede87d773e34a1ac417e2365a414bfe3
Rajesh <peketirajesh007@gmail.com>
2023-08-02 00:36:05 +0530

    cards over with grid

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,10 +1,14 @@
 const mainBar = document.querySelector("#bars")
 const logo = document.querySelector("#logo")
 const navLink = document.querySelectorAll(".nav-link")
+const header = document.querySelector("#header")
+const hero = document.querySelector(".hero")
 
 mainBar.onclick = () =>{
     logo.classList.toggle("active")
     navLink.forEach((e)=>{
         e.classList.toggle("active")
     })
+    header.classList.toggle("active")
+    hero.classList.toggle("active")
 }
